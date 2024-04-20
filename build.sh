ad6009d6334f94e88d2f795ebe9c681e22ba76f4
cvnertnc <148134890+cvnertnc@users.noreply.github.com>
2024-04-20 16:04:21 +0300

    `fix`

--- build.sh
+++ build.sh
@@ -167,10 +167,10 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases/latest/) for non-root YouTube and YT Music APKs"
+log "\nInstall [Microg](https://github.com/rufusin/GmsCore/releases/latest/) for non-root YouTube and YT Music APKs"
 log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
 log "\n[rvx-app](https://github.com/cvnertnc/rvx-app)"
-log "\nChangelog:"
+log "\n"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
 SKIPPED=$(cat $TEMP_DIR/skipped 2>/dev/null || :)
